45e4e7941b0d8abc566b9e9ed3ce6831096ebeea
gabriel-liraa <gabriel.l.lira26@gmail.com>
2023-04-04 18:12:09 -0300

    basic_nvim_config

---
+++ init.lua
@@ -0,0 +1,12 @@
+require("gabriellira")
+
+require('lualine').setup()
+
+require('lualine').setup {
+  options = {
+    theme = 'dracula',
+  },
+}
+
+vim.wo.number = true
+vim.wo.relativenumber = true
---
+++ lua/gabriellira/packer.lua
@@ -0,0 +1,34 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+
+	use 'wbthomason/packer.nvim'
+
+	use 'Mofiqul/dracula.nvim'
+
+	use('ThePrimeagen/harpoon')
+
+	use('mbbill/undotree')
+
+	use('tpope/vim-commentary')
+
+	use {
+		"windwp/nvim-autopairs",
+		config = function() require("nvim-autopairs").setup {} end
+	}
+
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+	}
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+end)
